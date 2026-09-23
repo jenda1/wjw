f7b1fea52c284fa357a649342c7380f83a0d4161
Jan Lána <lana.jan@gmail.com>
2026-09-23 20:51:50 +0200

    add filter to Agenda page

--- doc/templatetags/doc_tags.py
+++ doc/templatetags/doc_tags.py
@@ -8,3 +8,10 @@ register = template.Library()
 @register.filter(name='person_display')
 def do_person_display(person):
     return person_display(person)
+
+
+@register.filter(name='get_item')
+def get_item(struct_value, key):
+    """Přístup do StructValue podle jména pole zjištěného za běhu (např. z query stringu) -
+    `{{ item.item }}` v šabloně jde napsat jen s pevným jménem pole."""
+    return struct_value.get(key)
